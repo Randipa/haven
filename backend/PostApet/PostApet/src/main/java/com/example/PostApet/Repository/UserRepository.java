80e0460f25011443b5cfb4bdfe85405229e786cb
Saliya Pathum <96324718+Randipa@users.noreply.github.com>
2025-07-01 20:27:28 +0530

    Fix admin account creation when multiple admins

--- backend/PostApet/PostApet/src/main/java/com/example/PostApet/Repository/UserRepository.java
+++ backend/PostApet/PostApet/src/main/java/com/example/PostApet/Repository/UserRepository.java
@@ -8,10 +8,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findFirstByEmail(String email);
 
-    Optional<User> findByUserRole(UserRole userRole);
+    List<User> findAllByUserRole(UserRole userRole);
 }
