025cca61f0567c1a2b45055482d17389804185ca
Saliya Pathum <96324718+Randipa@users.noreply.github.com>
2025-07-01 23:24:46 +0530

    Revert "Fix admin dashboard auth handling"

--- frontend/src/adminPanel/Sidebar/Sidebar.js
+++ frontend/src/adminPanel/Sidebar/Sidebar.js
@@ -20,40 +20,27 @@ const Sidebar = ({ onLogout }) => {
         </div>
         <h2 className="sidebar-title">Paws Haven</h2>
         <ul className="sidebar-list">
-          <li>
-            <NavLink
-              to="/admin/dashboard"
-              end
-              className={({ isActive }) => (isActive ? "active-link" : "")}
-            >
+            <li>
+            <NavLink exact to="/admin/dashboard" activeClassName="active-link" >
               <FaTachometerAlt className="sidebar-icon" />
               Dashboard
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to="/admin/pets"
-              className={({ isActive }) => (isActive ? "active-link" : "")}
-            >
+            <NavLink exact to="/admin/pets" activeClassName="active-link">
               <FaPaw className="sidebar-icon" />
               Requested Pets
             </NavLink>
           </li>
-
-          <li>
-            <NavLink
-              to="/admin/users"
-              className={({ isActive }) => (isActive ? "active-link" : "")}
-            >
+         
+           <li>
+            <NavLink to ="/admin/users" activeClassName="active-link">
               <FaUser className="sidebar-icon" />
               User Management
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to="/admin/messages"
-              className={({ isActive }) => (isActive ? "active-link" : "")}
-            >
+            <NavLink to="/admin/messages" activeClassName="active-link">
               <FaEnvelope className="sidebar-icon" />
               Contact Messages
             </NavLink>
